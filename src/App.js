c3ec0bab3b9e3b2eb5896c6e91e43e73db20b401
MaestroShifu <sdanielsarsantos@gmail.com>
2019-07-15 16:55:34 -0500

    componentes funcionales / Componentes basados en clases

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hi, I´m a React App</h1>
-    </div>
-  );
+import Person from './Person/Person'
+
+//-> class-based components
+class App extends Component {
+  render() {
+    /* Forma tradicional de retornar */
+    return (
+      <div className="App">
+        <h1>Hi, I´m a React App</h1>
+        <p>This is really working!</p>
+
+        <Person />
+      </div>
+    );
+
+    /* Crear un elemento -> createElement('html', configuración, 'Nodos hijos') */
+    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, I´m a React App'));
+  }
 }
 
 export default App;
+ 
---
+++ src/Person/Person.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+//-> Functional components [Es la mejor practica]
+const person = () => {
+    return <p>I´m a Person!</p>
+};
+
+export default person
