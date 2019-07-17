0b6b9e6b7daff4267b6a3675302f0bd4cd60eaa0
MaestroShifu <sdanielsarsantos@gmail.com>
2019-07-16 21:56:24 -0500

    Conceptos basicos de React js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,19 @@ import Person from './Person/Person'
 
 //-> class-based components
 class App extends Component {
+  state = {
+    persons: [
+      { name: "Max", age: 28 },
+      { name: "Daniel", age: 19 },
+      { name: "Manu", age: 15 },
+    ]
+  }
+
+  //Si no lo llamo de esta manera, va a hacer referencia a la clase y no al evento como tal
+  switchNameHandler = () => {//-> Expresion de Handler da referencia a eventos [Buenas practicas]
+    console.log('Was cliked!');
+  }
+
   render() {
     /* Forma tradicional de retornar */
     return (
@@ -12,7 +25,11 @@ class App extends Component {
         <h1>Hi, I´m a React App</h1>
         <p>This is really working!</p>
 
-        <Person />
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}> My hobbies: Run in the park </Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
       </div>
     );
 
@@ -22,4 +39,10 @@ class App extends Component {
 }
 
 export default App;
+
+//Datos importantes
+
+//-> Props: Son los atributos de configuración para el componente.
+
+//-> State: Es la representacion del componente en un tiempo determinado, esta data puede variar en el ciclo de vida del mismo [Al detectar cambio renderiza en el DOM el componente]
  
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,8 +1,16 @@
 import React from 'react'
 
 //-> Functional components [Es la mejor practica]
-const person = () => {
-    return <p>I´m a Person!</p>
+const person = (props) => {
+    // return <p>I´m a Person and I am { Math.floor(Math.random() * 30) } years old!</p>//-> Para que el jsx interprete el js es con { }
+    
+    //-> Cuando son mas de dos lineas, se debe poner un div para agrupar y poner parentesis ( ).
+    return (
+        <div>
+            <p>I´m a { props.name } and I am { props.age } years old!</p>
+            <p>{ props.children }</p>
+        </div>
+    );//-> Cuando se usa una clase como componente se maneja [this.props] para acceder a ellos.
 };
 
 export default person
